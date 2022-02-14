76f486d9bf9a317795d2486329f5f60e95abe9d4
Frédérick AGATHE <fagathe77@gmail.com>
2022-02-14 21:11:37 +0100

    #75 Page show Page & #81 Isoler la template de Breadcrumb

--- src/Twig/AppExtension.php
+++ src/Twig/AppExtension.php
@@ -27,25 +27,54 @@ class AppExtension extends AbstractExtension
         return [
             new TwigFunction('socials', [$this, 'socials']),
             new TwigFunction('getClass', [$this, 'getClass']),
+            new TwigFunction('setClass', [$this, 'setClass']),
+            new TwigFunction('getType', [$this, 'getType']),
         ];
     }
 
     /**
      * @return null|Social[]
      */
-    public function socials(): array | null
+    public function socials(): ?array
     {
         return $this->socialRepository->findAll();
     }
 
-    public function getClass(object $object): ?string
+    /**
+     * @param mixed $param
+     *
+     * @return string|null
+     */
+    public function getClass(mixed $param): ?string
     {
-        if (is_object($object)) {
+        if (is_object($param)) {
 
-            return $object::class;
+            return (string) $param::class;
         }
 
         return null;
+    }
+
+    public function setClass(?string $string = null): ?string
+    {
+        if (is_string($string)) {
+            $string = preg_split('/(?=[A-Z])/', $string, -1, PREG_SPLIT_NO_EMPTY);
+            $string = join("\\", $string);
+
+            return $string;
+        }
+
+        return null;
+    }
+
+    /**
+     * @param mixed $var
+     *
+     * @return string
+     */
+    public function getType(mixed $var): string
+    {
+        return gettype($var);
     }
 
 }
